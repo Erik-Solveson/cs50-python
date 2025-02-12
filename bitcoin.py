c7f6d3b1a4546b0ee1c1f1f6fe02896178e20762
Erik-Solveson <erik.solveson@gmail.com>
2025-02-12 14:45:30 -0700

    finished problem 6 for problem set 4, bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -3,23 +3,31 @@
 import requests
 import sys
 
-API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
+API_URL = "http://api.coindesk.com/v1/bpi/currentprice"
+ALT_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 TEST_TRIVIA_URL = "http://jservice.io/api/random"
 
 
 def main():
-#     bitcoin_price = get_current_bitcoin_to_USD_price()
-    response = requests.get(API_URL)
-    # response.json()
-    print(response)
+    bitcoin_price = get_current_bitcoin_to_USD_price()
+    print_bitcoin_amount(bitcoin_price)
 
-# def get_current_bitcoin_to_USD_price():
-#     try:
-#         response = requests.get(TEST_TRIVIA_URL)
-#         print(response)
-#     except requests.RequestException as e:
-#         print(f"there was problem with your request {e}")
-#     except requests.ConnectTimeout:
-#         print(f"Your connection timed out")
+def get_current_bitcoin_to_USD_price():
+    try:
+        response = requests.get(ALT_URL)
+        data = response.json()
+        return data['bitcoin']['usd']
+    except requests.RequestException as e:
+        print(f"there was problem with your request {e}")
+    except requests.ConnectTimeout:
+        print(f"Your connection timed out")
+
+def print_bitcoin_amount(bitcoin_price):
+    try:
+        number = float(sys.argv[1])
+        print(f"${number*bitcoin_price:,.4f}")
+    except ValueError:
+        print("incorrect number of command line args")
+        sys.exit()
 
 main()
